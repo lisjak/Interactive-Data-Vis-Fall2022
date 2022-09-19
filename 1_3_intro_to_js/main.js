68a4447ee6e01079b5a2b996534fd95412ba11f8
Lisa Jakubczyk <lisajakubczyk@gmail.com>
2022-09-18 20:44:15 -0400

    tutorial 1.3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const jsConfetti = new JSConfetti();
+let count = 0;
+document.getElementById("messageButton").addEventListener("click", function() {
+    let message = document.getElementById("message");
+    document.getElementById('last-message').innerHTML = `“${message.value}”`;
+    count++;
+    document.getElementById('counter').innerHTML = `${count}`;
+    jsConfetti.addConfetti({
+        emojis: ['🌈', '⚡️', '✨', '💫', '🌸'],
+        emojiSize: 100,
+        emojiSize: 75,
+        confettiNumber: 60,
+    })
+});
